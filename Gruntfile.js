59a87ebc5b5be7f2f7bd18a37026f99b08b9fc76
Leonardo Val <leonardo.val@creatartis.com>
2018-11-13 13:28:33 -0300

    Project layout

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+﻿/** Gruntfile for [ludorum-player-assess.js](http://github.com/LeonardoVal/ludorum-player-assess.js).
+*/
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	});
+
+	require('@creatartis/creatartis-grunt').config(grunt, {
+		sourceNames: ['__prologue__',
+			'chi-squared',
+			'__epilogue__'],
+		deps: [
+			{ id: 'creatartis-base', name: 'base' },
+			{ id: 'sermat', name: 'Sermat',
+				path: 'node_modules/sermat/build/sermat-umd.js' },
+			{ id: 'ludorum' }
+		],
+		targets: {
+			build_umd: {
+				fileName: 'build/ludorum-player-assess',
+				wrapper: 'umd'
+			},
+			build_raw: {
+				fileName: 'build/ludorum-player-assess-tag',
+				wrapper: 'tag'
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['build']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ludorum-player-assess
-Player assessment in the Ludorum framework
+
+[![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
+
+Player assessment in the [Ludorum](https://github.com/LeonardoVal/ludorum.js) framework.
+
+## License
+
+Open source under an MIT license. See [LICENSE](LICENSE).
+
+## Development
+
+Development requires [NodeJS](http://nodejs.org/). Download the repository and run `npm install` to install all dependencies.
+
+## Contact
+
+This software is being continually developed. Suggestions and comments are always welcome via [email](mailto:leonardo.val@creatartis.com).
