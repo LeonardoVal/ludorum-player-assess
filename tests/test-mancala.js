e1d922330bb93ba2c801d29417e79e22f36f05db
LeonardoVal <leonardo.val@creatartis.com>
2018-11-30 21:40:36 -0300

    Added test for Mancala

---
+++ tests/test-mancala.js
@@ -0,0 +1,22 @@
+/**
+ 
+*/
+var base = require('creatartis-base'),
+	ludorum = require('ludorum'),
+	mancala = require('@creatartis/ludorum-game-mancala'),
+	assess = require('../build/ludorum-player-assess');
+
+var LOGGER = base.Logger.ROOT;
+LOGGER.appendToConsole();
+var RANDOM = new ludorum.players.RandomPlayer({ name: "RANDOM" }),
+	MCTS50 = new ludorum.players.MonteCarloPlayer({ name: "MCTS50", simulationCount: 50 }),
+	MCTS100 = new ludorum.players.MonteCarloPlayer({ name: "MCTS100", simulationCount: 100 }),
+	MANCALA = new mancala.Mancala(); 
+
+assess.config({ logger: LOGGER });
+assess.measure([RANDOM, MCTS50, MCTS100])
+.playing(MANCALA)
+.againstRandom({ matchCount: 40 })
+.then(function (r) {
+	console.log(r.Mancala);
+});
